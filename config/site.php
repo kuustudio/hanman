e82f52bad9d4b0529ff9e7971c27d64be15761bc
hiliqi <4780044@qq.com>
2018-12-21 11:52:59 +0800

    1

--- config/site.php
+++ config/site.php
@@ -7,10 +7,10 @@
  */
 
 return [
-    'url' => 'http://www.hanman.co',
+    'url' => '127.0.0.1',
     'site_name' => '御宅屋',
-    'admin' => 'hiliqi',
-    'password' => 'aiguo999',
+    'admin' => 'admin',
+    'password' => '123456',
     'xz_api' => 'http://data.zz.baidu.com/urls?appid=1613751078195175&token=ENXW9HZSzAefXPfj&type=realtime',
     'vip_count' => 10
 ];
